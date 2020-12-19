86253571a0c18fdb311ab6d577db15b51766b6ab
Rajessen Pillay <handersonscott21@gmail.com>
2020-12-19 19:27:31 +0400

    Added the SQL files compliant for MariaDB and Oracle

---
+++ builder/forever_mariadb.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 19, 2020 at 03:24 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `forever`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_cart`
+--
+
+DROP TABLE IF EXISTS `tbl_cart`;
+CREATE TABLE IF NOT EXISTS `tbl_cart` (
+  `cart_id` int(2) NOT NULL,
+  `cart_qoh` int(2) DEFAULT NULL,
+  `cart_product_qoh` int(2) DEFAULT NULL,
+  `user_id` int(2) DEFAULT NULL,
+  PRIMARY KEY (`cart_id`),
+  KEY `tbl_cart_user_id_fk` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_category`
+--
+
+DROP TABLE IF EXISTS `tbl_category`;
+CREATE TABLE IF NOT EXISTS `tbl_category` (
+  `cat_id` int(2) NOT NULL,
+  `cat_desc` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`cat_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_color`
+--
+
+DROP TABLE IF EXISTS `tbl_color`;
+CREATE TABLE IF NOT EXISTS `tbl_color` (
+  `color_id` int(2) NOT NULL,
+  `color` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`color_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_inventory`
+--
+
+DROP TABLE IF EXISTS `tbl_inventory`;
+CREATE TABLE IF NOT EXISTS `tbl_inventory` (
+  `inventory_id` int(2) NOT NULL,
+  `inv_color` varchar(255) DEFAULT NULL,
+  `inv_size` varchar(100) DEFAULT NULL,
+  `inv_qoh` int(3) DEFAULT NULL,
+  `inv_price` decimal(10,2) DEFAULT NULL,
+  `color_id` int(2) DEFAULT NULL,
+  `product_id` int(2) DEFAULT NULL,
+  PRIMARY KEY (`inventory_id`),
+  KEY `tbl_inventory_color_id` (`color_id`),
+  KEY `tbl_inventory_product_id` (`product_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_orders`
+--
+
+DROP TABLE IF EXISTS `tbl_orders`;
+CREATE TABLE IF NOT EXISTS `tbl_orders` (
+  `order_id` int(2) NOT NULL,
+  `o_sub_total` decimal(10,2) DEFAULT NULL,
+  `o_tax` int(2) DEFAULT NULL,
+  `o_shipping` varchar(255) DEFAULT NULL,
+  `o_methpayment` varchar(255) DEFAULT NULL,
+  `o_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(2) DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `tbl_orders_order_user_id_fk` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_order_line`
+--
+
+DROP TABLE IF EXISTS `tbl_order_line`;
+CREATE TABLE IF NOT EXISTS `tbl_order_line` (
+  `product_id` int(2) DEFAULT NULL,
+  `order_id` int(2) DEFAULT NULL,
+  `ol_quantity` int(3) DEFAULT NULL,
+  KEY `tbl_order_line_product_id_fk` (`product_id`),
+  KEY `tbl_order_line_order_id_fk` (`order_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product`
+--
+
+DROP TABLE IF EXISTS `tbl_product`;
+CREATE TABLE IF NOT EXISTS `tbl_product` (
+  `product_id` int(2) NOT NULL,
+  `prod_desc` varchar(255) DEFAULT NULL,
+  `prod_tags` varchar(255) DEFAULT NULL,
+  `prod_brand` varchar(255) DEFAULT NULL,
+  `prod_image` varchar(255) DEFAULT NULL,
+  `cat_id` int(2) DEFAULT NULL,
+  `cart_id` int(2) DEFAULT NULL,
+  PRIMARY KEY (`product_id`),
+  KEY `tbl_product_cat_id_fk` (`cat_id`),
+  KEY `tbl_product_cart_id_fk` (`cart_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+DROP TABLE IF EXISTS `tbl_user`;
+CREATE TABLE IF NOT EXISTS `tbl_user` (
+  `user_id` int(2) NOT NULL,
+  `username` varchar(60) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email_address` varchar(100) DEFAULT NULL,
+  `phone_number` int(8) DEFAULT NULL,
+  `postal_code` int(5) DEFAULT NULL,
+  `gender` varchar(20) DEFAULT NULL,
+  `age_group` varchar(20) DEFAULT NULL,
+  `account_status` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ builder/forever_oracle_version.sql
@@ -0,0 +1,97 @@
+-- Forever SQL
+DROP TABLE tbl_user CASCADE CONSTRAINTS;
+DROP TABLE tbl_cart CASCADE CONSTRAINTS;
+DROP TABLE tbl_category CASCADE CONSTRAINTS;
+DROP TABLE tbl_orders CASCADE CONSTRAINTS;
+DROP TABLE tbl_order_line CASCADE CONSTRAINTS;
+DROP TABLE tbl_product CASCADE CONSTRAINTS;
+DROP TABLE tbl_color CASCADE CONSTRAINTS;
+DROP TABLE tbl_inventory CASCADE CONSTRAINTS;
+
+CREATE TABLE tbl_user
+(
+  user_id NUMBER(2),
+  username VARCHAR2 (60) NOT NULL UNIQUE,
+  password VARCHAR2 (255) NOT NULL, -- Max length for password hashing
+  email_address VARCHAR2 (100),
+  phone_number NUMBER(8), -- Mauritius number format
+  postal_code  NUMBER(5), -- Maurtius postal code format
+  gender VARCHAR2 (20) CHECK (gender IN ('female', 'male')),
+  age_group VARCHAR2 (20) CHECK (age_group IN ('adult', 'child')),
+  account_status VARCHAR2 (10) CHECK ( account_status IN ('active', 'inactive')),
+  CONSTRAINT tbl_user_user_id_pk PRIMARY KEY (user_id)
+);
+
+CREATE TABLE tbl_cart
+(
+  cart_id NUMBER (2),
+  cart_qoh NUMBER (2),
+  cart_product_qoh NUMBER (2),
+  user_id NUMBER (2),
+  CONSTRAINT tbl_cart_cart_id_pk PRIMARY KEY (cart_id),
+  CONSTRAINT tbl_cart_user_id_fk FOREIGN KEY (user_id) REFERENCES tbl_user (user_id)
+);
+
+CREATE TABLE tbl_category
+(
+    cat_id NUMBER (2),
+    cat_desc VARCHAR2 (255),
+    CONSTRAINT tbl_category_cat_id_pk PRIMARY KEY ( cat_id )
+);
+
+CREATE TABLE tbl_product
+(
+  product_id NUMBER(2),
+  prod_desc VARCHAR2 (255),
+  prod_tags VARCHAR2 (255),
+  prod_brand VARCHAR2 (255),
+  prod_image VARCHAR2 (255),
+  cat_id NUMBER (2),
+  cart_id NUMBER (2),
+  CONSTRAINT tbl_product_product_id_pk PRIMARY KEY (product_id),
+  CONSTRAINT tbl_product_cat_id_fk FOREIGN KEY (cat_id) REFERENCES tbl_category (cat_id),
+  CONSTRAINT tbl_product_cart_id_fk FOREIGN KEY (cart_id) REFERENCES tbl_cart (cart_id)
+);
+
+CREATE TABLE tbl_orders
+(
+  order_id NUMBER (2),
+  o_sub_total NUMBER (10, 2),
+  o_tax NUMBER (2),
+  o_shipping VARCHAR2 (255),
+  o_methpayment VARCHAR2 (255) CHECK ( o_methpayment IN  ('CC', 'DB', 'VC', 'MC')),
+  o_date DATE DEFAULT SYSDATE,
+  user_id NUMBER (2),
+  CONSTRAINT tbl_orders_order_id_pk PRIMARY KEY (order_id),
+  CONSTRAINT tbl_orders_order_user_id_fk FOREIGN KEY (user_id) REFERENCES tbl_user ( user_id )
+);
+
+CREATE TABLE tbl_order_line
+(
+  product_id NUMBER (2),
+  order_id NUMBER (2),
+  ol_quantity NUMBER (3),
+  CONSTRAINT tbl_order_line_product_id_fk FOREIGN KEY (product_id) REFERENCES tbl_product ( product_id ),
+  CONSTRAINT tbl_order_line_order_id_fk FOREIGN KEY (order_id) REFERENCES tbl_orders ( order_id )
+);
+
+CREATE TABLE tbl_color
+(
+  color_id NUMBER (2),
+  color VARCHAR2 (10),
+  CONSTRAINT tbl_color_color_id_pk PRIMARY KEY ( color_id )
+);
+
+CREATE TABLE tbl_inventory
+(
+  inventory_id NUMBER (2),
+  inv_color VARCHAR2 (255),
+  inv_size VARCHAR2 (100) CHECK ( inv_size IN ('S', 'L', 'M')),
+  inv_qoh NUMBER (3),
+  inv_price NUMBER (10, 2),
+  color_id NUMBER (2),
+  product_id NUMBER (2),
+  CONSTRAINT tbl_inventory_inventory_id_pk PRIMARY KEY (inventory_id),
+  CONSTRAINT tbl_inventory_color_id FOREIGN KEY (color_id) REFERENCES tbl_color (color_id),
+  CONSTRAINT tbl_inventory_product_id FOREIGN KEY (product_id) REFERENCES tbl_product (product_id)
+);
