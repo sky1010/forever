2bef88d5c80930e14948a02f76e91ca3b9b71d4f
sky1010 <yashmungur1530@gmail.com>
2020-12-20 14:43:27 +0400

    sql product

--- builder/forever_mariadb.sql
+++ builder/forever_mariadb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Dec 19, 2020 at 08:46 PM
--- Server version: 5.7.26
--- PHP Version: 7.2.18
+-- Host: 127.0.0.1
+-- Generation Time: Dec 20, 2020 at 11:41 AM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,14 +28,11 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbl_cart`
 --
 
-DROP TABLE IF EXISTS `tbl_cart`;
-CREATE TABLE IF NOT EXISTS `tbl_cart` (
-  `cart_id` int(2) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_cart` (
+  `cart_id` int(2) NOT NULL,
   `cart_qoh` int(2) DEFAULT NULL,
   `cart_product_qoh` int(2) DEFAULT NULL,
-  `user_id` int(2) DEFAULT NULL,
-  PRIMARY KEY (`cart_id`),
-  KEY `tbl_cart_user_id_fk` (`user_id`)
+  `user_id` int(2) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -44,25 +41,20 @@ CREATE TABLE IF NOT EXISTS `tbl_cart` (
 -- Table structure for table `tbl_category`
 --
 
-DROP TABLE IF EXISTS `tbl_category`;
-CREATE TABLE IF NOT EXISTS `tbl_category` (
-  `cat_id` int(2) NOT NULL AUTO_INCREMENT,
-  `cat_desc` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`cat_id`)
+CREATE TABLE `tbl_category` (
+  `cat_id` int(2) NOT NULL,
+  `cat_desc` varchar(255) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `tbl_color`
+-- Dumping data for table `tbl_category`
 --
 
-DROP TABLE IF EXISTS `tbl_color`;
-CREATE TABLE IF NOT EXISTS `tbl_color` (
-  `color_id` int(2) NOT NULL AUTO_INCREMENT,
-  `color` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`color_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+INSERT INTO `tbl_category` (`cat_id`, `cat_desc`) VALUES
+(1, 'Clothing'),
+(2, 'Footwear'),
+(3, 'Accesories'),
+(4, 'Cosmetics');
 
 -- --------------------------------------------------------
 
@@ -70,18 +62,13 @@ CREATE TABLE IF NOT EXISTS `tbl_color` (
 -- Table structure for table `tbl_inventory`
 --
 
-DROP TABLE IF EXISTS `tbl_inventory`;
-CREATE TABLE IF NOT EXISTS `tbl_inventory` (
-  `inventory_id` int(2) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_inventory` (
+  `inventory_id` int(2) NOT NULL,
   `inv_color` varchar(255) DEFAULT NULL,
   `inv_size` varchar(100) DEFAULT NULL,
   `inv_qoh` int(3) DEFAULT NULL,
   `inv_price` decimal(10,2) DEFAULT NULL,
-  `color_id` int(2) DEFAULT NULL,
-  `product_id` int(2) DEFAULT NULL,
-  PRIMARY KEY (`inventory_id`),
-  KEY `tbl_inventory_color_id` (`color_id`),
-  KEY `tbl_inventory_product_id` (`product_id`)
+  `product_id` int(2) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -90,17 +77,14 @@ CREATE TABLE IF NOT EXISTS `tbl_inventory` (
 -- Table structure for table `tbl_orders`
 --
 
-DROP TABLE IF EXISTS `tbl_orders`;
-CREATE TABLE IF NOT EXISTS `tbl_orders` (
-  `order_id` int(2) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_orders` (
+  `order_id` int(2) NOT NULL,
   `o_sub_total` decimal(10,2) DEFAULT NULL,
   `o_tax` int(2) DEFAULT NULL,
   `o_shipping` varchar(255) DEFAULT NULL,
   `o_methpayment` varchar(255) DEFAULT NULL,
   `o_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_id` int(2) DEFAULT NULL,
-  PRIMARY KEY (`order_id`),
-  KEY `tbl_orders_order_user_id_fk` (`user_id`)
+  `user_id` int(2) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -109,14 +93,11 @@ CREATE TABLE IF NOT EXISTS `tbl_orders` (
 -- Table structure for table `tbl_order_line`
 --
 
-DROP TABLE IF EXISTS `tbl_order_line`;
-CREATE TABLE IF NOT EXISTS `tbl_order_line` (
+CREATE TABLE `tbl_order_line` (
   `product_id` int(2) DEFAULT NULL,
   `order_id` int(2) DEFAULT NULL,
   `ol_quantity` int(3) DEFAULT NULL,
-  `ol_postal_code` int(5) DEFAULT NULL,
-  KEY `tbl_order_line_product_id_fk` (`product_id`),
-  KEY `tbl_order_line_order_id_fk` (`order_id`)
+  `ol_postal_code` int(5) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -125,20 +106,16 @@ CREATE TABLE IF NOT EXISTS `tbl_order_line` (
 -- Table structure for table `tbl_product`
 --
 
-DROP TABLE IF EXISTS `tbl_product`;
-CREATE TABLE IF NOT EXISTS `tbl_product` (
-  `product_id` int(2) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_product` (
+  `product_id` int(2) NOT NULL,
+  `prod_name` varchar(120) NOT NULL,
   `prod_desc` varchar(255) DEFAULT NULL,
   `prod_tags` varchar(255) DEFAULT NULL,
   `prod_brand` varchar(255) DEFAULT NULL,
   `prod_image` varchar(255) DEFAULT NULL,
-  `prod_gender` varchar(60) DEFAULT NULL,
   `prod_age_group` varchar(100) DEFAULT NULL,
   `cat_id` int(2) DEFAULT NULL,
-  `cart_id` int(2) DEFAULT NULL,
-  PRIMARY KEY (`product_id`),
-  KEY `tbl_product_cat_id_fk` (`cat_id`),
-  KEY `tbl_product_cart_id_fk` (`cart_id`)
+  `cart_id` int(2) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -147,25 +124,117 @@ CREATE TABLE IF NOT EXISTS `tbl_product` (
 -- Table structure for table `tbl_user`
 --
 
-DROP TABLE IF EXISTS `tbl_user`;
-CREATE TABLE IF NOT EXISTS `tbl_user` (
-  `user_id` int(2) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_user` (
+  `user_id` int(2) NOT NULL,
   `username` varchar(60) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email_address` varchar(100) DEFAULT NULL,
   `account_status` varchar(10) DEFAULT NULL,
   `avatar` varchar(255) NOT NULL,
-  `role` varchar(255) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+  `role` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_user`
 --
 
 INSERT INTO `tbl_user` (`user_id`, `username`, `password`, `email_address`, `account_status`, `avatar`, `role`) VALUES
-(25, 'Pills', 'Pillsnet21!', 'pills@gmail.com', 'active', '../uploads/users_avatar/default_avatar.jpg', 'client');
+(25, 'pills', 'YASHveer2510#', 'pills@gmail.com', 'inactive', '../uploads/users_avatar/default_avatar.jpg', 'client'),
+(26, 'sky', 'YASHveer2510#', 'skyrider2531@gmail.cpm', 'active', '../uploads/users_avatar/default_avatar.jpg', 'admin'),
+(27, 'beast', 'YASHveer2510#', 'beast@gmail.com', 'active', '../uploads/users_avatar/default_avatar.jpg', 'client');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_cart`
+--
+ALTER TABLE `tbl_cart`
+  ADD PRIMARY KEY (`cart_id`),
+  ADD KEY `tbl_cart_user_id_fk` (`user_id`);
+
+--
+-- Indexes for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `tbl_inventory`
+--
+ALTER TABLE `tbl_inventory`
+  ADD PRIMARY KEY (`inventory_id`),
+  ADD KEY `tbl_inventory_product_id` (`product_id`);
+
+--
+-- Indexes for table `tbl_orders`
+--
+ALTER TABLE `tbl_orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `tbl_orders_order_user_id_fk` (`user_id`);
+
+--
+-- Indexes for table `tbl_order_line`
+--
+ALTER TABLE `tbl_order_line`
+  ADD KEY `tbl_order_line_product_id_fk` (`product_id`),
+  ADD KEY `tbl_order_line_order_id_fk` (`order_id`);
+
+--
+-- Indexes for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `tbl_product_cat_id_fk` (`cat_id`),
+  ADD KEY `tbl_product_cart_id_fk` (`cart_id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_cart`
+--
+ALTER TABLE `tbl_cart`
+  MODIFY `cart_id` int(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  MODIFY `cat_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_inventory`
+--
+ALTER TABLE `tbl_inventory`
+  MODIFY `inventory_id` int(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_orders`
+--
+ALTER TABLE `tbl_orders`
+  MODIFY `order_id` int(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  MODIFY `product_id` int(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `user_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
